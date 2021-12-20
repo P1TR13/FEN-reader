4ef71c9614043aa1a49ffef38dc168d7269de0c3
P1TR13 <pawel.michalowski@onet.pl>
2021-12-20 11:12:16 +0100

    Added board and input

---
+++ script.js
@@ -0,0 +1,49 @@
+let board = document.querySelector("#board")
+
+let boxes = [['1','1','1','1','1','1','1','1','1','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','0','0','0','0','0','0','0','0','1'],
+            ['1','1','1','1','1','1','1','1','1','1']]
+
+function gettingBoxes() {
+    for (i = 1; i <= 8; i++) {
+        for (j = 1; j <= 8; j++) {
+            boxes[i][j] = document.querySelector("#p" + i.toString() + j.toString())
+        }
+    }
+}
+
+
+function createBoard() {
+    for (i = 1; i <= 8; i++) {
+        for (j = 1; j <= 8; j++) {
+            let square = document.createElement("div")
+            square.setAttribute("id", "p" + i.toString() + j.toString())
+            if (i % 2 == 1) {
+                if (j % 2 == 1) {
+                    square.setAttribute("class", "square white")
+                } else {
+                    square.setAttribute("class", "square black")
+                }
+            } else {
+                if (j % 2 == 0) {
+                    square.setAttribute("class", "square white")
+                } else {
+                    square.setAttribute("class", "square black")
+                }
+            }
+
+            board.appendChild(square)
+
+        }
+    }
+}
+
+createBoard()
+gettingBoxes()
